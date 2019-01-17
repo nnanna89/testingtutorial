04e1daa22c001f8f6f8f37b6b8a5f145967c2b89
Nanalash <nmadu@seamfix.com>
2019-01-17 18:10:05 +0100

    Added arquillian tests

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+e
+5src/test/java/org/arquillian/example/GreeterTest.java,d/4/d442dd422791be4ddd7980aa6b2f25b217e6a75c
+a
+1src/main/java/org/arquillian/example/Greeter.java,b/6/b66b7458796a3c5c30fe11a721a0cac845def423
---
+++ src/main/java/org/arquillian/example/Greeter.java
@@ -0,0 +1,17 @@
+package org.arquillian.example;
+
+import java.io.PrintStream;
+
+/**
+ * A component for creating personal greetings
+ */
+public class Greeter {
+
+    public void greet(PrintStream to, String name){
+        to.println(name);
+    }
+
+    public String createGreeting(String name){
+        return "Hello, " + name + "!";
+    }
+}
